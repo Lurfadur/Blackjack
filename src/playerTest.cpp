6f98fdd5f3ff1b9fafab4f744e00ba55f64308f4
octesian <octesian@gmail.com>
2016-02-02 14:25:42 -0800

    added several Player methods, implemented playerTest

---
+++ src/playerTest.cpp
@@ -0,0 +1,69 @@
+#include "Player.h"
+#include "Deck.h"
+
+using std::cout;
+using std::endl;
+
+int main(){
+
+ //make a deck and deal to players; 
+  Deck newDeck;
+                                                              
+  std::cout << "Card Count: " << newDeck.size() << std::endl;
+  std::cout << "Shuffling... " << std::endl;
+  newDeck.shuffle();
+  
+  Player* p = new Player(1000);
+
+  p->addCard(newDeck.getCard());
+  p->addCard(newDeck.getCard());
+  
+  p->addBet(15);
+  cout << "Player P: \n";
+  p->printStatus();
+  cout << endl;
+
+  Player* q = new Player(p);
+  cout << "Player P: \n";
+  p->printStatus();
+  cout << endl;
+  cout << "Player Q: \n";
+  q->printStatus();
+  cout << endl;
+  
+  q->push();
+
+  cout << "Player Q: \n";
+  q->printStatus();
+  cout << endl;
+  q->addBet(15);
+
+  q->winBet(1.5);
+
+  cout << "Player Q: \n";
+  q->printStatus();
+  cout << endl;
+
+  q->unsplit(); 
+  std::cout << "after unsplit\n";
+  cout << "Player P: \n";
+  p->printStatus();
+  cout << endl;
+  cout << "Player Q: \n";
+  q->printStatus();
+  cout << endl;
+
+  p->insure();
+  std::cout << "insure\n";
+  cout << "Player P: \n";
+  p->printStatus();
+  cout << endl;
+
+  p->winInsure();
+  std::cout << "win insure\n";
+  cout << "Player P: \n";
+  p->printStatus();
+  cout << endl;
+  delete(q);
+  delete(p);
+}
